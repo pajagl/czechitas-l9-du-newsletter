642a9e2f055e00bd6664d115be4ef3ef3d3dbbf2
Pavlina Glozygova <pglozygova@gmail.com>
2023-03-23 18:33:59 +0100

    modified:   main.js

--- main.js
+++ main.js
@@ -1,55 +1,5 @@
 // tady je místo pro náš program
 
-// Auto
-const auto = document.querySelector('#auto');
-
-const move = (event) => {
-    const tlacitko = event.key;
-
-    if(tlacitko === 'ArrowRight') {
-        auto.style.transform = 'rotateY(0deg)';
-        auto.style.animation = 'to-right 1s forwards';
-    }
-
-    if(tlacitko === 'ArrowLeft') {
-        auto.style.transform = 'rotateY(180deg)';
-        auto.style.animation = 'to-left 1s forwards';
-    }
-  
-    //console.log(event);
-    console.log('keydown');
-    console.log(tlacitko);
-}
-
-window.addEventListener('keydown', move);
-
-// Přihlášení 
-const user = {
-	email: 'bartolomej.rozumbrada@gmail.com',
-	password: 'vimzenicnevim',
-	name: 'Bartoloměj',
-}
-
-document.querySelector('#login-email').addEventListener('submit', (e) => {
-	e.preventDefault()
-
-	const napisEmail = document.querySelector('#email')
-	const vlozHeslo = document.querySelector('#password')
-	const container = document.querySelector('.container')
-	const status = document.querySelector('#status')
-
-	if (
-		napisEmail.value === user.email &&
-		vlozHeslo.value === user.password
-	) {
-		container.innerHTML = `<h1>Přihlášený uživatel: ${user.name}</h1>`
-	} else {
-		status.textContent = 'Neplatné přihlašovací údaje'
-		vlozHeslo.value = ''
-	}
-})
-
-
 // Newsletter
 const formular = document.querySelector('form')
 
