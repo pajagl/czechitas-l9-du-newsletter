de3e93027667afe469a55d39f53c89ddeff0b0b1
Pavlina Glozygova <pglozygova@gmail.com>
2023-03-23 20:56:00 +0100

    modified:   index.html modified:   main.js modified:   style.css

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 // Newsletter
 const formular = document.querySelector('form')
+const textovepolicko = document.querySelector('input')
 
 const odebirat = (event) => {
 	event.preventDefault()
@@ -10,5 +11,14 @@ const odebirat = (event) => {
 	formular.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
 }
 
+const kontrolaPolicka = (event) => {
+	if (textovepolicko.value === '' || !textovepolicko.value.includes('@'), textovepolicko.value === '' || !textovepolicko.value.includes('.')) {
+		textovepolicko.classList.add('alertInput')
+	} else {
+		textovepolicko.classList.remove('alertInput')
+	}
+}
+
 formular.addEventListener('submit', odebirat)
+textovepolicko.addEventListener('input', kontrolaPolicka)
 
